c5a46e9b37acba840b3af1d2379c84d2adebe8f1
Shaarif Asim <shaarifasim0900@gmail.com>
2022-06-23 17:36:19 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,149 @@
+
+
+// function abc(){
+//     var positivenumber = document.getElementById('positive-integer').value;
+    
+//     var num = Math.abs(positivenumber);
+
+//     var roundoff = Math.round(num);
+
+//     var floorvalue = Math.floor(num);
+
+//     var ceilvalue = Math.ceil(num);
+
+//     document.getElementById('anumber').innerHTML = num ;
+//     document.getElementById('roundoffvalue').innerHTML = roundoff ;
+//     document.getElementById('floorvalue').innerHTML = floorvalue ;
+//     document.getElementById('ceilvalue').innerHTML = ceilvalue ;
+
+//     console.log('anumber');
+// }
+
+
+//negative integer//
+// function xyz(){
+//     var positivenumber = document.getElementById('positive-integer').value;
+    
+//     var num = -Math.abs(positivenumber);
+
+//     var roundoff = Math.round(num);
+
+//     var floorvalue = Math.floor(num);
+
+//     var ceilvalue = Math.ceil(num);
+
+//     document.getElementById('anumber').innerHTML = num ;
+//     document.getElementById('roundoffvalue').innerHTML = roundoff ;
+//     document.getElementById('floorvalue').innerHTML = floorvalue ;
+//     document.getElementById('ceilvalue').innerHTML = ceilvalue ;
+
+//     console.log('anumber');
+// }
+
+
+// positive floating point
+
+// function xyz(){
+//         var positivenumber = document.getElementById('positive-integer').value;
+        
+//         var num = parseFloat(positivenumber);
+    
+//         var roundoff = Math.round(num);
+    
+//         var floorvalue = Math.floor(num);
+    
+//         var ceilvalue = Math.ceil(num);
+    
+//         document.getElementById('anumber').innerHTML = num ;
+//         document.getElementById('roundoffvalue').innerHTML = roundoff ;
+//         document.getElementById('floorvalue').innerHTML = floorvalue ;
+//         document.getElementById('ceilvalue').innerHTML = ceilvalue ;
+    
+//         console.log('anumber');
+//     }
+
+
+
+// negative floating point
+
+// function xyz(){
+//     var positivenumber = document.getElementById('positive-integer').value;
+    
+//     var num = -parseFloat(positivenumber);
+
+//     var roundoff = Math.round(num);
+
+//     var floorvalue = Math.floor(num);
+
+//     var ceilvalue = Math.ceil(num);
+
+//     document.getElementById('anumber').innerHTML = num ;
+//     document.getElementById('roundoffvalue').innerHTML = roundoff ;
+//     document.getElementById('floorvalue').innerHTML = floorvalue ;
+//     document.getElementById('ceilvalue').innerHTML = ceilvalue ;
+
+//     console.log('anumber');
+// }
+
+
+
+// AbsoluteValue
+// function xyz(){
+//     var positivenumber = document.getElementById('positive-integer').value;    
+//     var num = Math.abs(positivenumber);
+//     document.getElementById('anumber').innerHTML = num ;
+//     console.log('anumber');
+// }
+
+// Random Dice value
+// function xyz(){
+//     var num = document.getElementById('dice-roll').value;
+//     document.getElementById('Rnumber').innerHTML = num ;
+// var diceRoll = Math.floor( Math.random() * 6 ) +1;
+// alert('You rolled a ' + diceRoll);
+// }
+
+
+// var heads = 0;
+// var tails = 0;
+// function click() {  
+//     var x = (Math.floor(Math.random() * 2) == 0);
+//     document.getElementById('click').innerHTML = x;
+//     if(x){
+//     	flip("heads");
+//     }else{
+//         flip("tails");
+//     }
+// };
+// function flip(coin) {
+//     document.getElementById("result").innerHTML = coin;
+// };
+
+
+
+// var rightNow = new Date();
+// document.getElementById('date').innerText = rightNow;
+
+
+// function show_now(){
+//     var my_month=new Date()
+    
+    
+//     var month_name=new Array(12);
+//     month_name[0]="January"
+//     month_name[1]="February"
+//     month_name[2]="March"
+//     month_name[3]="April"
+//     month_name[4]="May"
+//     month_name[5]="June"
+//     month_name[6]="July"
+//     month_name[7]="August"
+//     month_name[8]="September"
+//     month_name[9]="October"
+//     month_name[10]="November"
+//     month_name[11]="December"
+    
+//     alert ("Current month = " + month_name[my_month.getMonth()]); 
+//     }
+
+
